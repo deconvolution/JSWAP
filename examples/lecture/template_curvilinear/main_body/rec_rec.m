f4afba231c05f5b4e68a5f3ba7346d43b9d1955c
Yi Zhang <zhang9@outlook.de>
2021-11-29 00:37:05 +0100

    update examples

--- examples/lecture/template_curvilinear/main_body/rec_rec.m
+++
@@ -1,6 +0,0 @@
-clear all;
-close all;
-tt=load('./rec/rec_1.mat');
-rec_1=tt.data;
-figure;
-plot(rec_1);
---
+++ examples/lecture/template_staircase/main_body/rec/read_rec.m
@@ -0,0 +1,24 @@
+clear all;
+close all;
+dt=10^-3;
+nt=1000;
+%% load data
+tt=load('./rec_1.mat');
+V1=tt.data;
+tt=load('./rec_2.mat');
+V2=tt.data;
+tt=load('./rec_3.mat');
+V3=tt.data;
+nr=size(V3,2);
+%% plot data
+figure;
+for i=1:nr
+    subplot(1,nr,i);
+    plot(V1(:,i),dt:dt:dt*nt);
+    set(gca,'ydir','reverse');
+    xlabel('v1 [m/s]');
+    if i==1
+        ylabel('t [s]');
+    end
+end
+
