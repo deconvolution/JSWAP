b07a0eb5610342a715f18683237cfe507457612b
Yi Zhang <zhang9@outlook.de>
2021-12-13 13:51:18 +0100

    add

---
+++ examples/wavefield_campi/campi_heterogenous_random_3Hz/JSWAP_initialization.jl
@@ -0,0 +1,53 @@
+## import packages
+using JSWAP
+Threads.nthreads()
+## input struct
+mutable struct input3
+    dt
+    dx
+    dy
+    dz
+    nt
+    nx
+    ny
+    nz
+    X
+    Y
+    Z
+    lambda
+    mu
+    rho
+    inv_Qa
+    r1
+    r2
+    r3
+    s1
+    s2
+    s3
+    M11
+    M22
+    M33
+    M23
+    M13
+    M12
+    r1t
+    r2t
+    r3t
+    Rm
+    s1t
+    s2t
+    s3t
+    lp
+    nPML
+    Rc
+    PML_active
+    plot_interval
+    wavefield_interval
+    path
+    path_pic
+    path_model
+    path_wavefield
+    path_rec
+end
+input2=input3(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,);
